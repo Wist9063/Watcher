4081dc21263a0c358fc247ab6f89865c7bd4be05
Wist9063 <wist9063@gmail.com>
2019-01-30 21:27:18 -0800

    Added ellint

--- commands/Information/serverinfo.js
+++ commands/Information/serverinfo.js
@@ -16,7 +16,7 @@ module.exports = class extends Command {
         .setDescription(`This server has `)
         .setURL("https://discord.gg/EH7jKFH")
         .addField("❯❯ General Information", `Servers - \`${this.client.guilds.size.toLocaleString()}\`\nUsers - \`${this.client.users.size.toLocaleString()}\`\nUptime - \`${this.client.convertTime(this.client.uptime)}\``, true)
-        .addField("❯❯ Module Information", `Discord.js - \`${Discord.version}\`\nNode.js - \`${process.version}\``, true)
+        .addField("❯❯ Module Information", `\`\nNode.js - \`${process.version}\``, true)
         .addField("❯❯ Additional Information", `Heap Usage - \`${bytes(process.memoryUsage().heapUsed)}\``)
 
         message.channel.send(embed)
