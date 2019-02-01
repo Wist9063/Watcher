6ac1d0e35d2e4a87537d1941f63073ff01fcc257
Wist9063 <wist9063@gmail.com>
2019-01-31 21:43:24 -0800

    Game cycle

--- events/Main/ready.js
+++ events/Main/ready.js
@@ -10,6 +10,25 @@ module.exports = class extends BotEvent {
 
   execute() {
     console.log(log.info(`[Discord] Client Info: \nUser: ${this.user.tag}\nGuilds: ${this.guilds.size}\nChannels: ${this.channels.size}\nUsers: ${this.users.size}`));
-    this.user.setPresence({ activity: { name: 'w!help | V1.0.0', type: 0 } });
+    this.user.game_cycle = () => {
+      const games = [
+        'dat boi | @Jennifer help',
+        `${this.guilds.size} guilds! | @Jennifer help`,
+        'wist\'s problems pass by! | @Jennifer help',
+        'errors | @Jennifer help',
+        'the poppin\' errors | @Jennifer help',
+        `${this.users.size} users! | @Jennifer help`,
+        'my website | @Jennifer help',
+        'wumpus | @Jennifer help',
+        'you | @Jennifer help',
+        'fortnite streams | @Jennifer help',
+        'my code | @Jennifer help',
+        'my spotify playlist | @Jennifer help'
+      ];
+      this.user.setActivity(games[Math.round(Math.random() * (games.length - 1))], {'url': 'https://www.twitch.tv/monstercat', 'type': 'WATCHING' });
+      return setTimeout(() => {
+        this.user.game_cycle();
+      }, 300000);
+    };
   }
 };
