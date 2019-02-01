ccb387e2b24751c7c2b62d10bee1eafbfc8bfb48
Wist9063 <wist9063@gmail.com>
2019-01-31 21:49:26 -0800

    Replace Jennifer

--- events/Main/ready.js
+++ events/Main/ready.js
@@ -10,20 +10,20 @@ module.exports = class extends BotEvent {
 
   execute() {
     console.log(log.info(`[Discord] Client Info: \nUser: ${this.user.tag}\nGuilds: ${this.guilds.size}\nChannels: ${this.channels.size}\nUsers: ${this.users.size}`));
-    this.user.game_cycle = () => {
+    this.game_cycle = () => {
       const games = [
-        'dat boi | @Jennifer help',
-        `${this.guilds.size} guilds! | @Jennifer help`,
-        'wist\'s problems pass by! | @Jennifer help',
-        'errors | @Jennifer help',
-        'the poppin\' errors | @Jennifer help',
-        `${this.users.size} users! | @Jennifer help`,
-        'my website | @Jennifer help',
-        'wumpus | @Jennifer help',
-        'you | @Jennifer help',
-        'fortnite streams | @Jennifer help',
-        'my code | @Jennifer help',
-        'my spotify playlist | @Jennifer help'
+        'your server. | w!help',
+        `${this.guilds.size} guilds! | w!help`,
+        'wist\'s logs. | w!help',
+        'stuff. | w!help',
+        'netflix. | w!help',
+        `${this.users.size} users! | w!help`,
+        'my server. | w!help',
+        'discord. | w!help',
+        'twitch. | w!help',
+        'my mail. | w!help',
+        'my code. | w!help',
+        'youtube. | w!help'
       ];
       this.user.setActivity(games[Math.round(Math.random() * (games.length - 1))], {'url': 'https://www.twitch.tv/monstercat', 'type': 'WATCHING' });
       return setTimeout(() => {
