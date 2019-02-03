02552bf4da93a4006de7898005ce1191a06939cf
Wist9063 <wist9063@gmail.com>
2019-02-02 21:07:13 -0800

    testing

--- events/Main/ready.js
+++ events/Main/ready.js
@@ -10,6 +10,23 @@ module.exports = class extends BotEvent {
 
   execute() {
     console.log(log.info(`[Discord] Client Info: \nUser: ${this.user.tag}\nGuilds: ${this.guilds.size}\nChannels: ${this.channels.size}\nUsers: ${this.users.size}`));
-    this.client.user.setActivity('your server. | w!help', {'url': 'https://www.twitch.tv/monstercat', 'type': 'WATCHING' });
+    this.setActivity('your server. | w!help', {'url': 'https://www.twitch.tv/monstercat', 'type': 'WATCHING' });
+    setTimeout(() => {
+      const games = [
+        'your server. | w!help',
+        `${this.guilds.size} guilds! | w!help`,
+        'wist\'s logs. | w!help',
+        'stuff. | w!help',
+        'netflix. | w!help',
+        `${this.users.size} users! | w!help`,
+        'my server. | w!help',
+        'discord. | w!help',
+        'twitch. | w!help',
+        'my mail. | w!help',
+        'my code. | w!help',
+        'youtube. | w!help'
+      ];
+      this.setActivity(games[Math.round(Math.random() * (games.length - 1))], {'url': 'https://www.twitch.tv/monstercat', 'type': 'WATCHING' });
+    }, 300000);
   }
 };
