9027dad042ab3158543df75419764ce53b14f23c
Wist9063 <wist9063@gmail.com>
2019-02-02 21:01:10 -0800

    why n game cycl

--- events/Main/ready.js
+++ events/Main/ready.js
@@ -10,22 +10,6 @@ module.exports = class extends BotEvent {
 
   execute() {
     console.log(log.info(`[Discord] Client Info: \nUser: ${this.user.tag}\nGuilds: ${this.guilds.size}\nChannels: ${this.channels.size}\nUsers: ${this.users.size}`));
-    setTimeout(() => {
-      const games = [
-        'your server. | w!help',
-        `${this.guilds.size} guilds! | w!help`,
-        'wist\'s logs. | w!help',
-        'stuff. | w!help',
-        'netflix. | w!help',
-        `${this.users.size} users! | w!help`,
-        'my server. | w!help',
-        'discord. | w!help',
-        'twitch. | w!help',
-        'my mail. | w!help',
-        'my code. | w!help',
-        'youtube. | w!help'
-      ];
-      this.client.user.setActivity(games[Math.round(Math.random() * (games.length - 1))], {'url': 'https://www.twitch.tv/monstercat', 'type': 'WATCHING' });
-    }, 300000);
+    this.client.user.setActivity('your server. | w!help', {'url': 'https://www.twitch.tv/monstercat', 'type': 'WATCHING' });
   }
 };
