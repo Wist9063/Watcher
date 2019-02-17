922929ea01c3723862349f551164bed03e166e8f
Josh Arcenal <josharcenal@gmail.com>
2019-02-16 23:49:53 -0800

    removing useless functions

--- index.js
+++ index.js
@@ -23,15 +23,6 @@ new class extends Client {
     log.info('[Discord] Connecting to Discord..');
   }
 
-  async getAllShardsAvailable() {
-    try {
-      await this.shard.fetchClientValues('ping');
-    } catch (e) {
-      return false;
-    }
-    return true;
-  }
-
   reloadCommands() {
     console.log('Command reload triggered.');
     klaw(commandsPath).on('data', item => {
@@ -72,25 +63,6 @@ new class extends Client {
     });
   }
 
-  convertTime(t) {
-    const absoluteSeconds = parseInt((t / (1000)) % 60);
-    const absoluteMinutes = parseInt((t / (1000 * 60)) % 60);
-    const absoluteHours = parseInt((t / (1000 * 60 * 60)) % 24);
-    const absoluteDays = parseInt((t / (1000 * 60 * 60 * 24)));
-
-    const d = absoluteDays > 0 ? absoluteDays === 1 ? '1 day' : `${absoluteDays} days` : null;
-    const h = absoluteHours > 0 ? absoluteHours === 1 ? '1 hour' : `${absoluteHours} hours` : null;
-    const m = absoluteMinutes > 0 ? absoluteMinutes === 1 ? '1 minute' : `${absoluteMinutes} minutes` : null;
-    const s = absoluteSeconds > 0 ? absoluteSeconds === 1 ? '1 second' : `${absoluteSeconds} seconds` : null;
-
-    const absoluteTime = [];
-    if (d) absoluteTime.push(d);
-    if (h) absoluteTime.push(h);
-    if (m) absoluteTime.push(m);
-    if (s) absoluteTime.push(s);
-
-    return absoluteTime.join(', ');
-  }
 };
 
 process.on('uncaughtException', err => log.error(err.stack, true));
