6c1cffc12ecb9cdf705e7d97109a30f53cdda282
Wist9063 <wist9063@gmail.com>
2019-02-26 18:51:34 -0800

    lol updates

--- events/Main/guildDelete.js
+++ events/Main/guildDelete.js
@@ -1,5 +1,5 @@
 const BotEvent = require('../../handlers/event.js');
-const { MessageEmbed } = require('discord.js');
+const { WebhookClient, MessageEmbed } = require('discord.js');
 const moment = require('moment');
 
 module.exports = class extends BotEvent {
@@ -10,10 +10,15 @@ module.exports = class extends BotEvent {
   }
 
   async execute(guild) {
-    const logChannel = this.channels.get('506299766955835392');
-    if (!logChannel) return;
+
+    const hook = new WebhookClient('549476222686461972', this.config.wenhookToken);
+
     const embed = new MessageEmbed()
-      .setColor('#D92C2C').setTitle('Guild Delete').setURL('https://discord.gg/EH7jKFH').setDescription(`Removed from ${guild.name} (ID:${guild.id}), which is owned by ${guild.owner.user.tag} (ID:${guild.owner.user.id}), has ${guild.memberCount} members, and ${guild.members.filter(mem => mem.user.bot).size} bots.\n\n\`\`\`autohotkey\n${moment(guild.createdAt).format('MMMM Do, YYYY, h:mm:ss A')}\`\`\``).setFooter(`ID: ${guild.id}`);
-    return logChannel.send(embed);
+      .setColor('#D92C2C')
+      .setTitle('Guild Delete')
+      .setURL('https://discord.gg/EH7jKFH')
+      .setDescription(`Watcher now at ***${this.client.guilds.size.toLocaleString()}*** guilds. Removed from ${guild.name} (ID:${guild.id}), which is owned by ${guild.owner.user.tag} (ID:${guild.owner.user.id}), has ${guild.memberCount} members, and ${guild.members.filter(mem => mem.user.bot).size} bots.\n\n\`\`\`autohotkey\n${moment(guild.createdAt).format('MMMM Do, YYYY, h:mm:ss A')}\`\`\``)
+      .setFooter(`ID: ${guild.id}`);
+    return hook.send(embed);
   }
 };
