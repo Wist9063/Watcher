063b1e5572470dcbf4e66b7c92d42a229ab27eef
Wist9063 <wist9063@gmail.com>
2019-04-27 00:41:47 -0700

    added pull.js

---
+++ commands/Staff/pull.js
@@ -0,0 +1,17 @@
+const Command = require('../../handlers/command.js');
+const exec = require('child_process').exec;
+
+module.exports = class extends Command {
+  constructor(client, filePath) {
+    super(client, filePath, {
+      name: 'pull',
+      aliases: []
+    });
+  }
+
+  execute(message) {
+    if (message.perm < 9) return;
+    message.channel.send(`${this.client.emojis.get('503081230309392384')}**Pulling From Github...**`).then(msg => setTimeout(() => { msg.edit(`${this.client.emojis.get('506673019838660608')}**Client successfully restarted.**`);},1500));
+    setTimeout(() => { exec('git pull').catch(a => console.log(a));  }, 2500);
+  }
+};
