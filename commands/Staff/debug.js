4c87bebf316611724e7314faaef9c368c47b1d80
Wist9063 <wist9063@gmail.com>
2019-04-27 20:50:08 -0700

    fixing pull.js

--- commands/Staff/debug.js
+++ commands/Staff/debug.js
@@ -1,5 +1,6 @@
 const Command = require('../../handlers/command.js');
 const db = require('quick.db');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Command {
   constructor(client, filePath) {
@@ -11,8 +12,12 @@ module.exports = class extends Command {
 
   execute(message) {
     if (message.perm < 9) return;
-    let a = JSON.stringify(db.get(`guild_${message.guild.id}`));
-    a = JSON.parse(a);
-    message.channel.send(`\`\`\`js\n${a}\n\`\`\``);
+    const embed = new MessageEmbed()
+      .setTitle('Database Debug Viewer 9000')
+      .setDescription(`Currently accessing ${message.guild.name}'s database entry. Is enabled? ${db.get(`guild_${message.guild.id}.enabled`)}`)
+      .setURL('https://discord.gg/83SAWkh')
+      .addField('❯❯ Log Channel DB', `\`\`\`js\n${db.get(`guild_${message.guild.id}.logChannel`)}\n\`\`\``)
+      .addField('❯❯ Events DB', `\`\`\`js\n${db.get(`guild_${message.guild.id}.events`)}\n\`\`\``);
+    return message.channel.send(embed).catch(e => message.channel.send(`\`\`\`${e}\`\`\``));
   }
 };
