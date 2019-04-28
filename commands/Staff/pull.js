3b92c62518cf08b2e955590f4a75a5320790ab64
Wist9063 <wist9063@gmail.com>
2019-04-27 20:53:02 -0700

    was

--- commands/Staff/pull.js
+++ commands/Staff/pull.js
@@ -12,6 +12,6 @@ module.exports = class extends Command {
   execute(message) {
     if (message.perm < 9) return;
     message.channel.send(`${this.client.emojis.get('503081230309392384')}**Pulling From Github...**`).then(msg => setTimeout(() => { msg.edit(`${this.client.emojis.get('506673019838660608')}**Client successfully restarted.**`);},1500));
-    setTimeout(() => { exec('git pull').then(a => message.channel.send(`\`\`\`js\n${a}\n\`\`\``)), 2500;});
+    setTimeout(() => { exec('git pull').then(a => message.channel.send(`\`\`\`js\n${a}\n\`\`\``)); }, 2500);
   }
 };
