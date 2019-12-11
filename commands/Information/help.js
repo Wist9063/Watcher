bf5bd5e343397e31d81e6c4a88210580b1f88ed7
wist9063 <wist9063@gmail.com>
2019-12-10 18:09:35 -0800

    Reformatting help cmd

--- commands/Information/help.js
+++ commands/Information/help.js
@@ -13,7 +13,7 @@ module.exports = class extends Command {
     const embed = new MessageEmbed()
       .setColor('#7289DA')
       .setTitle('Watcher | The Advanced Logging Bot')
-      .setDescription('**Prefix**: `w!`\n\nWatcher provides your server with feature-rich advanced customizable logging features, to enhance your user experience.\n\nEvents we currently support:\nchannelCreate, channelDelete, guildBanAdd, guildBanRemove, guildMemberAdd, guildMemberRemove, guildMemberUpdate, messageDelete, messageReactionAdd, messageReactionRemove, messageUpdate, voiceStateUpdate')
+      .setDescription('**Prefix**: `w!`\n\nWatcher provides your server with feature-rich advanced customizable logging features, to enhance your user experience.\n\nEvents we currently support:\nchannelCreate, channelDelete, guildBanAdd, guildBanRemove, guildMemberAdd, guildMemberRemove, guildMemberUpdate, messageDelete, messageReactionAdd, messageReactionRemove, messageUpdate, voiceStateUpdate\n\n__*Special thanks to Nettle#2222 for keeping the project alive!*__')
       .setURL('https://discord.gg/83SAWkh')
       .setThumbnail(this.client.user.displayAvatarURL())
       .addField('Information [5]', '`settings`, `help`, `invite`, `userinfo`, `info`')
@@ -23,7 +23,7 @@ module.exports = class extends Command {
       .addField('Support Server', '[https://discord.gg/83SAWkh](https://discord.gg/83SAWkh)')
       .addField('Donate', '[https://www.patreon.com/watcherbot](https://www.patreon.com/watcherbot)')
       .addField('Setup Guide', 'Run the command `w!setup`.')
-      .setFooter('Originally maintained by jason.#0001, now maintained by Wistful__#9063. Made with the Discord.js library. On Runtime Node.js. Special thanks to Nettle#2222 for keeping the project alive!');
+      .setFooter('Originally made by jason.#0001, now maintained by Wistful_#9063. *made with love and keystrokes* :)');
     return message.channel.send(embed);
   }
 };
