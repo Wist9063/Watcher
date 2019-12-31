b8968709d5079ca82936d72920c153ac5d8283a8
wist9063 <wist9063@gmail.com>
2019-12-31 00:33:44 -0800

    Added version

--- commands/Information/info.js
+++ commands/Information/info.js
@@ -4,6 +4,7 @@ const bytes = require('bytes');
 const moment = require('moment');
 require('moment-duration-format');
 const db = require('quick.db');
+const pkg = require('../../package.json');
 
 module.exports = class extends Command {
   constructor(client, filePath) {
@@ -20,7 +21,7 @@ module.exports = class extends Command {
       .setDescription(`Quick overview, ${this.client.user.username} is in ${this.client.guilds.size.toLocaleString()} servers, has ${this.client.users.size.toLocaleString()} users globally, and obtains an uptime of ${duration}.`)
       .setURL('https://discord.gg/83SAWkh')
       .addField('❯❯ General Information', `Servers - \`${this.client.guilds.size.toLocaleString()}\`\nUsers - \`${this.client.users.size.toLocaleString()}\`\nUptime - \`${duration}\`\nDatabase Entries - \`${db.all().length}\``, true)
-      .addField('❯❯ Module Information', `Discord.js - \`${Discord.version}\`\nNode.js - \`${process.version}\`\nDatabase Version - \`${db.version}\``, true)
+      .addField('❯❯ Module Information', `Discord.js - \`${Discord.version}\`\nNode.js - \`${process.version}\`\nDatabase Version - \`${db.version}\`\nWatcher Version - \`${pkg.version}\``, true)
       .addField('❯❯ Additional Information', `\nMemory Usage - \`${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)}\`MB\nHeap Usage - \`${bytes(process.memoryUsage().heapUsed)}\``);
     return message.channel.send(embed).catch(e => message.channel.send(`\`\`\`${e}\`\`\``));
   }
