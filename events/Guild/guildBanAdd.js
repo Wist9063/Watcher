0220f70f85bd93db2b65882b7d327f5816084563
wist9063 <wist9063@gmail.com>
2020-03-23 12:54:02 -0700

    Updated embed and errors

--- events/Guild/guildBanAdd.js
+++ events/Guild/guildBanAdd.js
@@ -1,6 +1,7 @@
 const BotEvent = require('../../handlers/event.js');
 const db = require('quick.db');
 const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
 
 module.exports = class extends BotEvent {
   constructor(client, filePath) {
@@ -17,8 +18,13 @@ module.exports = class extends BotEvent {
       if (fetched === null) return;
       const logChannel = guild.channels.get(fetched);
       if (!logChannel) return;
+      const member = guild.member(user);
       const embed = new MessageEmbed()
-        .setColor('#D92C2C').setTitle('Member Banned').setURL('https://discord.gg/83SAWkh').setDescription(`${user.tag} (ID:${user.id}) was banned.`).setFooter(`ID: ${user.id}`).setTimestamp();
+        .setColor('#D92C2C')
+        .setAuthor(`${user.tag} has been banned.`, user.displayAvatarURL(), 'https://discord.gg/83SAWkh')
+        .setDescription(`**${user.tag}** has been banned in this server. This member joined the server at\n\`${moment.utc(member.joinedTimestamp).format('MMMM Do YYYY, h:mm:ss A')} (Universal Coordinated Time)\` and was banned at \`${moment.utc(new Date).format('MMMM Do YYYY, h:mm:ss A')} (Universal Coordinated Time)\``)
+        .setFooter(`${user.tag}'s ID is ${user.id}.`)
+        .setTimestamp();
       return logChannel.send(embed);
     } else {
       return;
--- events/Guild/voiceStateUpdate.js
+++ events/Guild/voiceStateUpdate.js
@@ -23,26 +23,28 @@ module.exports = class extends BotEvent {
 
       if (voice1 == 'None') {
         const embed = new MessageEmbed()
-          .setColor('#7289DA')
-          .setTitle('Voice State Update')
-          .setURL('https://discord.gg/83SAWkh')
-          .setDescription(`${newState.member.user.tag} (ID:${newState.member.user.id}) joined voice channel **${newState.channel.name}**.`)
+          .setColor('#32CD32')
+          .setAuthor(`${newState.member.user.tag} has joined a voice channel.`, newState.member.user.displayAvatarURL(), 'https://discord.gg/83SAWkh')
+          .setDescription(`**${newState.member.user.tag}** joined voice channel **${voice2}** in the category **${newState.channel.parent.name}**.`)
+          .setFooter(`${newState.member.user.tag}'s ID is ${newState.member.user.id} • This event is from a Voice State Update.`)
           .setTimestamp();
         return logChannel.send(embed);
       } else if (voice2 == 'Left') {
         const embed = new MessageEmbed()
           .setColor('#D92C2C')
-          .setTitle('Voice State Update')
-          .setURL('https://discord.gg/83SAWkh')
-          .setDescription(`${newState.member.user.tag} (ID:${newState.member.user.id}) left voice channel **${voice1}**.`)
+          .setAuthor(`${newState.member.user.tag} has left a voice channel.`, newState.member.user.displayAvatarURL(), 'https://discord.gg/83SAWkh')
+          .setDescription(`**${oldState.member.user.tag}** left voice channel **${voice1}** in the category **${oldState.channel.parent.name}**.`)
+          .setFooter(`${newState.member.user.tag}'s ID is ${newState.member.user.id} • This event is from a Voice State Update.`)
           .setTimestamp();
         return logChannel.send(embed);
       } else if (voice1 != newState.channel.name) {
+        let newPar;
+        if (oldState.channel.parent.name === newState.channel.parent.name) {newPar = 'same category';} else {newPar = `category **${newState.channel.parent.name}**`;}
         const embed = new MessageEmbed()
-          .setColor('#D92C2C')
-          .setTitle('Voice State Update')
-          .setURL('https://discord.gg/83SAWkh')
-          .setDescription(`${newState.member.user.tag} (ID:${newState.member.user.id}) moved from **${voice1}** -> **${newState.channel.name}**.`)
+          .setColor('#006400')
+          .setDescription(`${oldState.member.user.tag} has moved from **${voice1}** in the category **${oldState.channel.parent.name}** to **${newState.channel.name}** in the ${newPar}.`)
+          .setAuthor(`**${newState.member.user.tag}** has moved to another voice channel.`, newState.member.user.displayAvatarURL(), 'https://discord.gg/83SAWkh')
+          .setFooter(`${newState.member.user.tag}'s ID is ${newState.member.user.id} • This event is from a Voice State Update.`)
           .setTimestamp();
         return logChannel.send(embed);
       }
