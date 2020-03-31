8d7c6964fed393bd03c81bbbe07c4122c97dfc04
wist9063 <wist9063@gmail.com>
2020-03-30 20:12:53 -0700

    removed .toLocal method

--- commands/Information/info.js
+++ commands/Information/info.js
@@ -18,9 +18,9 @@ module.exports = class extends Command {
     const embed = new Discord.MessageEmbed()
       .setColor('#7289DA')
       .setTitle(`${this.client.user.tag} | General Statistics`)
-      .setDescription(`Quick overview, ${this.client.user.username} is in ${this.client.guilds.size.toLocaleString()} servers, has ${this.client.users.size.toLocaleString()} users globally, and obtains an uptime of ${duration}.`)
+      .setDescription(`Quick overview, ${this.client.user.username} is in ${this.client.guilds.size} servers, has ${this.client.users.size} users globally, and obtains an uptime of ${duration}.`)
       .setURL('https://discord.gg/83SAWkh')
-      .addField('❯❯ General Information', `Servers - \`${this.client.guilds.cache.size.toLocaleString()}\`\nUsers - \`${this.client.users.cache.size.toLocaleString()}\`\nUptime - \`${duration}\`\nDatabase Entries - \`${db.all().length}\``, true)
+      .addField('❯❯ General Information', `Servers - \`${this.client.guilds.cache.size}\`\nUsers - \`${this.client.users.cache.size}\`\nUptime - \`${duration}\`\nDatabase Entries - \`${db.all().length}\``, true)
       .addField('❯❯ Module Information', `Discord.js - \`${Discord.version}\`\nNode.js - \`${process.version}\`\nDatabase Version - \`${db.version}\`\nWatcher Version - \`${pkg.version}\``, true)
       .addField('❯❯ Additional Information', `\nMemory Usage - \`${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)}\`MB\nHeap Usage - \`${bytes(process.memoryUsage().heapUsed)}\``);
     return message.channel.send(embed).catch(e => message.channel.send(`\`\`\`${e}\`\`\``));
