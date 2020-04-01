a33a263fca9f363824e0e879a22c09af8a8b657f
wist9063 <wist9063@gmail.com>
2020-03-31 17:28:19 -0700

    add ability to check other guild settings

--- commands/Information/settings.js
+++ commands/Information/settings.js
@@ -11,27 +11,34 @@ module.exports = class extends Command {
   }
 
   execute(message) {
+    const content = message.content.split(' ')[1];
+
+    // Perm Check
+    if (message.perm <= 0) return message.channel.send(`${message.author} | Insufficient permissions required to execute this command.`);
+    if (content && message.perm <= 9) return message.channel.send(`${message.author} | Insufficient permissions required to execute this part of the command. Remove the arguments and try again.`);
+
+    if (content && !/^\d+$/.test(content)) return message.channel.send(`${message.author} | This isnt a valid number! Check arugments and try again.`);
+    if (content && !this.client.guilds.resolve(content)) return message.channel.send(`${message.author} | This is not a valid guild ID! Check arugments and try again.`);
+
     const check = this.client.emojis.resolve('506673019838660608');
     const tick = this.client.emojis.resolve('506673020014952448');
-
-    if (message.perm <= 0) return message.channel.send(`${message.author} | Insufficient permissions required to execute this command.`).then(msg => msg.delete({timeout:15000}));
-        
+      
     // LIST OF EVENTS
-    let logChannel = db.get(`guild_${message.guild.id}.logChannel.id`);
-    //let textLog = db.get(`guild_${message.guild.id}.textLog`);
-    let channelCreate = db.get(`guild_${message.guild.id}.events.channelCreate`);
-    let channelDelete = db.get(`guild_${message.guild.id}.events.channelDelete`);
-    let guildBanAdd = db.get(`guild_${message.guild.id}.events.guildBanAdd`);
-    let guildBanRemove = db.get(`guild_${message.guild.id}.events.guildBanRemove`);
-    let guildMemberAdd = db.get(`guild_${message.guild.id}.events.guildMemberAdd`);
-    let guildMemberUpdate = db.get(`guild_${message.guild.id}.events.guildMemberUpdate`);
-    let guildMemberRemove = db.get(`guild_${message.guild.id}.events.guildMemberRemove`);
-    let messageDelete = db.get(`guild_${message.guild.id}.events.messageDelete`);
-    let messageUpdate = db.get(`guild_${message.guild.id}.events.messageUpdate`);
-    let voiceStateUpdate = db.get(`guild_${message.guild.id}.events.voiceStateUpdate`);
-    let messageReactionAdd = db.get(`guild_${message.guild.id}.events.messageReactionAdd`);
-    let messageReactionRemove = db.get(`guild_${message.guild.id}.events.messageReactionRemove`);
-    let roleCreate = db.get(`guild_${message.guild.id}.events.roleCreate`);
+    let logChannel = db.get(`guild_${content ? content : message.guild.id}.logChannel.id`);
+    //let textLog = db.get(`guild_${content ? content : message.guild.id}.textLog`);
+    let channelCreate = db.get(`guild_${content ? content : message.guild.id}.events.channelCreate`);
+    let channelDelete = db.get(`guild_${content ? content : message.guild.id}.events.channelDelete`);
+    let guildBanAdd = db.get(`guild_${content ? content : message.guild.id}.events.guildBanAdd`);
+    let guildBanRemove = db.get(`guild_${content ? content : message.guild.id}.events.guildBanRemove`);
+    let guildMemberAdd = db.get(`guild_${content ? content : message.guild.id}.events.guildMemberAdd`);
+    let guildMemberUpdate = db.get(`guild_${content ? content : message.guild.id}.events.guildMemberUpdate`);
+    let guildMemberRemove = db.get(`guild_${content ? content : message.guild.id}.events.guildMemberRemove`);
+    let messageDelete = db.get(`guild_${content ? content : message.guild.id}.events.messageDelete`);
+    let messageUpdate = db.get(`guild_${content ? content : message.guild.id}.events.messageUpdate`);
+    let voiceStateUpdate = db.get(`guild_${content ? content : message.guild.id}.events.voiceStateUpdate`);
+    let messageReactionAdd = db.get(`guild_${content ? content : message.guild.id}.events.messageReactionAdd`);
+    let messageReactionRemove = db.get(`guild_${content ? content : message.guild.id}.events.messageReactionRemove`);
+    let roleCreate = db.get(`guild_${content ? content : message.guild.id}.events.roleCreate`);
 
     // FETCH VALUES
     if (logChannel) logChannel = `<#${logChannel}>`;
@@ -71,6 +78,6 @@ module.exports = class extends Command {
       .addField('🗒 Guild Settings', `**guildBanAdd**: ${guildBanAdd}\n**guidBanRemove**: ${guildBanRemove}\n**guildMemberAdd**: ${guildMemberAdd}\n**guildMemberUpdate**: ${guildMemberUpdate}\n**guildMemberRemove**: ${guildMemberRemove}\n **roleCreate**: ${roleCreate}`, true)
       .setFooter('w!<setting> <value> - Edit a setting, or run w!enable-all to turn all settings on, and w!disable-all to turn all settings off. This message will delete in 30 seconds.');
 
-    return message.channel.send(`${check} **Displaying module information for \`${message.guild.name}\`**.`, embed).then(msg => msg.delete({timeout:30000}));
+    return message.channel.send(`${check} **Displaying module information for \`${content ? this.client.guilds.resolve(content).name : message.guild.name}\`**.`, embed);
   }
 };
