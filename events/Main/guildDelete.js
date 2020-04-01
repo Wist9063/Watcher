4d16383501b0740cd5cc7fc885121a35f31f5ab5
wist9063 <wist9063@gmail.com>
2020-03-31 17:35:14 -0700

    Fixed guild join/leave bug

--- events/Main/guildDelete.js
+++ events/Main/guildDelete.js
@@ -24,7 +24,7 @@ module.exports = class extends BotEvent {
       .setColor('#D92C2C')
       .setTitle('Guild Delete')
       .setURL('https://discord.gg/83SAWkh')
-      .setDescription(`Watcher now at **${this.guilds.size}** guilds. Removed from ${guild.name} (ID:${guild.id}), which is owned by ${guild.owner.user.tag} (ID:${guild.owner.user.id}), has ${guild.memberCount} members, and ${guild.members.filter(mem => mem.user.bot).size} bots.\n\n\`\`\`autohotkey\n${moment(guild.createdAt).format('MMMM Do, YYYY, h:mm:ss A')}\`\`\``)
+      .setDescription(`Watcher now at **${this.guilds.size}** guilds. Removed from ${guild.name} (ID:${guild.id}), which is owned by ${guild.owner.user.tag} (ID:${guild.owner.user.id}), has ${guild.memberCount} members, and ${guild.members.cache.filter(mem => mem.user.bot).size} bots.\n\n\`\`\`autohotkey\n${moment(guild.createdAt).format('MMMM Do, YYYY, h:mm:ss A')}\`\`\``)
       .setFooter(`ID: ${guild.id}`);
     return hook.send(embed);
   }
