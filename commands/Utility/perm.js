fe0f935f0f7f3731fb67ba5fddb750c831970f90
wist9063 <wist9063@gmail.com>
2020-04-01 21:15:25 -0700

    changed permission handling

--- commands/Utility/perm.js
+++ commands/Utility/perm.js
@@ -11,24 +11,26 @@ module.exports = class extends Command {
   }
 
   execute(message) {
+    let match = /^<@!?(\d+)>$/.test(message.content.split(' ')[1]);
 
-    const match = /(?:perm)\s{0,}(?:(?:(?:<@!?)?(\d{17,20})>?))?/i.exec(message.content);
-    if (!match) return message.channel.send('Invalid Usage: `w!perm [mention]`');
+    // Checks
+    if (!/^<@!?(\d+)>$/.test(message.content.split(' ')[1])) match = message.author;
+    if (!message.mentions.users) return message.channel.send(`${message.author} | You didn't mention a user. Usage: \`w!perm [@user]\``);
+    if (/^<@!?(\d+)>$/.test(message.content.split(' ')[1]) && message.mentions.users) match = message.mentions.users.first(); 
 
-    let array, level, def;
+    let level, def;
 
-    if (!match[1]) array = Permissions.fetch(this.client, message);
-    if (match[1]) array = Permissions.fetchOther(this.client, message);
+    const array = Permissions.fetch(match, message);
     if (array !== 'err') {
       level = array[0];
       def = array[1];
-    } else return message.channel.send('An error has occured.');
+    } else { return message.channel.send(`${message.author} | An error has occured while fetching the permissions. Please try again later.`); }
 
     const embed = new MessageEmbed()
-      .setTitle(`Permission Level for ${match[1] ? message.guild.members.get(match[1]).user.username : message.author.username}`)
-      .setDescription(`${def}\n**Permission Level:** **${level}**`)
+      .setTitle(`Permission Level for ${message.mentions.users ? message.mentions.users.first().username : message.author.username}`)
+      .setDescription(`**${def}**\nPermission Level: __${level}__`)
       .setFooter(`Requested by ${message.author.tag}`)
       .setColor(0xcc8822);
-    message.channel.send(embed);
+    return message.channel.send(embed);
   }
 };
