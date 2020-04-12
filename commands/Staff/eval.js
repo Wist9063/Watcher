6101d1d37077d4124599d6357e7c51f75e7eb56f
wist9063 <wist9063@gmail.com>
2020-04-11 19:11:06 -0700

    added extra protection for eval cmd

--- commands/Staff/eval.js
+++ commands/Staff/eval.js
@@ -1,7 +1,16 @@
 const Command = require('../../handlers/command.js');
-const MessageEmbed = require('discord.js').MessageEmbed;
 const config = require('../../config.js');
-// const  = require('../../handlers/.js');
+
+function clean(text) {
+  text = text.toString();
+  if (text.includes(config.token)) {
+    text = text.replace(config.token, 'PLACEHOLDER FOR TOKEN.');
+  }
+  if (typeof(text) === 'string') {
+    return text.replace(/`/g, `\`${String.fromCharCode(8203)}`).replace(/@/g, `@${String.fromCharCode(8203)}`);
+  }
+  return text;
+}
 
 module.exports = class extends Command {
   constructor(client, filePath) {
@@ -9,42 +18,23 @@ module.exports = class extends Command {
       name: 'ev',
       aliases: ['eval']
     });
-    this.embed = function(input, output, error = false) {
-      return new MessageEmbed().setColor(error ? 0xFF0000 : 0x00FF00).addField(':desktop: Input', input).addField(error ? ':error: Error' : 'Output', `\`\`\`${error ? '' : 'js'}\n${output}\n\`\`\``).setFooter(`${this.client.user.username} Eval`);
-    };
   }
+  
 
   async execute(message) {
     if (message.perm < 9) return;
     const a = message.content.slice(message.content.search(' ') + 1);
-    if (!a) {
-      return message.channel.send('You must provide some code to evaluate!');
-    }
+    if (!a) return message.channel.send('You must provide some code to evaluate!');
   
     try {
       let res = eval(a);
       res = await res;
       if (typeof res !== 'string') res = require('util').inspect(res);
-
-      // .log('warn', `Eval Args: ${a}`);
-      console.log(`[Eval] Args: ${a}`);
-            
-  
+      console.log(`[Eval by ${message.author.tag}] Args: ${a}`); 
       await message.channel.send('```js\n' + '> ' + clean(res) + '\n```');
     } catch (e) {
       message.channel.send('```js\n' + '> ' + e + '\n```');
     }
 
-
-  }};
-
-function clean(text) {
-  text = text.toString();
-  if (text.includes(config.token)) {
-    text = text.replace(config.token, 'reeeeeeeeeeeee don\'t try to take my token');
   }
-  if (typeof(text) === 'string') {
-    return text.replace(/`/g, `\`${String.fromCharCode(8203)}`).replace(/@/g, `@${String.fromCharCode(8203)}`);
-  }
-  return text;
-}
+};
