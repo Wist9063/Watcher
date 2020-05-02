319d1263cada00db73d3dec4138b05e59d418364
wist9063 <wist9063@gmail.com>
2020-05-02 01:14:56 -0700

    added support for mongodb to all guild events

--- events/Guild/guildMemberUpdate.js
+++ events/Guild/guildMemberUpdate.js
@@ -1,6 +1,6 @@
 const BotEvent = require('../../handlers/event.js');
-const db = require('quick.db');
-const { MessageEmbed } = require('discord.js');
+const db = new (require('../../handlers/database.js'))();
+const { MessageEmbed, WebhookClient } = require('discord.js');
 
 module.exports = class extends BotEvent {
   constructor(client, filePath) {
@@ -11,42 +11,45 @@ module.exports = class extends BotEvent {
 
   async execute(oldMember, newMember) {
     if (oldMember.user.bot) return;
-    const fetched = await db.get(`guild_${newMember.guild.id}.logChannel.id`);
-    const fetch = await db.get(`guild_${newMember.guild.id}.events.channelDelete`);
-    if (fetch === null) return;
-    if (fetch === true) {
-      if (fetched === null) return;
-      const logChannel = newMember.guild.channels.cache.get(fetched);
-      if (!logChannel) return;
-      if (oldMember.nickname != newMember.nickname) {
-        const embed = new MessageEmbed()
-          .setColor('#7289DA')
-          .setTitle('Member Updated | Nickname')
-          .setURL('https://discord.gg/83SAWkh')
-          .setDescription(`${newMember.user.tag} (ID:${newMember.user.id}) edited their nickname.`)
-          .setFooter(`ID: ${newMember.user.id}`)
-          .setTimestamp()
-          .addField('Previous Nickname', oldMember.nickname, true)
-          .addField('Current Nickname', newMember.nickname, true);
-        return logChannel.send(embed);
-      } else if (oldMember.roles.size != newMember.roles.size) {
-        const oldRoles = oldMember.roles.map(r => r).join(' ').replace('@everyone', ' ');
-        if (oldRoles.length > 99) oldRoles.substring(0, 100) + ', and more.';
-        const newRoles = newMember.roles.map(r => r).join(' ').replace('@everyone', ' ');
-        if (newRoles.length > 99) newRoles.substring(0, 100) + ', and more.';
-        const embed = new MessageEmbed()
-          .setColor('#7289DA')
-          .setTitle('Member Updated | Roles')
-          .setURL('https://discord.gg/83SAWkh')
-          .setDescription(`${newMember.user.tag} (ID:${newMember.user.id}) edited their roles.`)
-          .addField('Previous Roles', `󠂪󠂪${oldRoles}`)
-          .addField('Current Roles', `󠂪󠂪${newRoles}`)
-          .setFooter(`ID: ${newMember.user.id}`)
-          .setTimestamp();
-        return logChannel.send(embed);
+    await db.get(newMember.guild.id, this.mongod, 'events').then((a) => {
+      if (a.events.guildMemberUpdate === null) return;
+      if (a.events.guildMemberUpdate === true) {
+        db.get(newMember.guild.id, this.mongod, 'guildSettings').then((b) => {
+          if (b.wb.wbID === null || b.wb.wbKey === null) return;
+          const logChannel = new WebhookClient(b.wb.wbID, b.wb.wbKey);
+          if (!logChannel) return;
+
+          if (oldMember.nickname != newMember.nickname) {
+            const embed = new MessageEmbed()
+              .setColor('#7289DA')
+              .setTitle('Member Updated | Nickname')
+              .setURL('https://discord.gg/83SAWkh')
+              .setDescription(`${newMember.user.tag} (ID:${newMember.user.id}) edited their nickname.`)
+              .setFooter(`ID: ${newMember.user.id}`)
+              .setTimestamp()
+              .addField('Previous Nickname', oldMember.nickname === null ? oldMember.user.tag : oldMember.nickname, true)
+              .addField('Current Nickname', newMember.nickname === null ? newMember.user.tag : newMember.nickname, true);
+            return logChannel.send(embed);
+          } else if (oldMember.roles.cache.size != newMember.roles.cache.size) {
+            const oldRoles = oldMember.roles.cache.map(r => r).join(' ').replace('@everyone', ' ');
+            if (oldRoles.length > 99) oldRoles.substring(0, 100) + ', and more.';
+            const newRoles = newMember.roles.cache.map(r => r).join(' ').replace('@everyone', ' ');
+            if (newRoles.length > 99) newRoles.substring(0, 100) + ', and more.';
+            const embed = new MessageEmbed()
+              .setColor('#7289DA')
+              .setTitle('Member Updated | Roles')
+              .setURL('https://discord.gg/83SAWkh')
+              .setDescription(`${newMember.user.tag} (ID:${newMember.user.id}) edited their roles.`)
+              .addField('Previous Roles', `󠂪󠂪${oldRoles}`)
+              .addField('Current Roles', `󠂪󠂪${newRoles}`)
+              .setFooter(`ID: ${newMember.user.id}`)
+              .setTimestamp();
+            return logChannel.send(embed);
+          }
+        });
+      } else {
+        return;
       }
-    } else {
-      return;
-    }
+    });
   }
 };
--- events/Guild/voiceStateUpdate.js
+++ events/Guild/voiceStateUpdate.js
@@ -1,6 +1,6 @@
 const BotEvent = require('../../handlers/event.js');
-const db = require('quick.db');
-const { MessageEmbed } = require('discord.js');
+const db = new (require('../../handlers/database.js'))();
+const { MessageEmbed, WebhookClient } = require('discord.js');
 
 module.exports = class extends BotEvent {
   constructor(client, filePath) {
@@ -10,46 +10,47 @@ module.exports = class extends BotEvent {
   }
 
   async execute(oldState, newState) {
-    const fetched = await db.get(`guild_${oldState.guild.id}.logChannel.id`);
-    const fetch = await db.get(`guild_${oldState.guild.id}.events.voiceStateUpdate`);
-    if (fetch === null) return;
-    if (fetch === true) {
-      if (fetched === null) return;
-      const logChannel = oldState.guild.channels.cache.get(fetched);
-      if (!logChannel) return;
+    await db.get(oldState.guild.id, this.mongod, 'events').then((a) => {
+      if (a.events.voiceStateUpdate === null) return;
+      if (a.events.voiceStateUpdate === true) {
+        db.get(oldState.guild.id, this.mongod, 'guildSettings').then((b) => {
+          if (b.wb.wbID === null || b.wb.wbKey === null) return;
+          const logChannel = new WebhookClient(b.wb.wbID, b.wb.wbKey);
+          if (!logChannel) return;
+          const voice1 = (oldState.channel ? oldState.channel.name : 'None');
+          const voice2 = (newState.channel ? newState.channel.name : 'Left');
 
-      const voice1 = (oldState.channel ? oldState.channel.name : 'None');
-      const voice2 = (newState.channel ? newState.channel.name : 'Left');
-
-      if (voice1 == 'None') {
-        const embed = new MessageEmbed()
-          .setColor('#32CD32')
-          .setAuthor(`${newState.member.user.tag} has joined a voice channel.`, newState.member.user.displayAvatarURL(), 'https://discord.gg/83SAWkh')
-          .setDescription(`**${newState.member.user.tag}** joined voice channel **${voice2}** in the category **${newState.channel.parent.name}**.`)
-          .setFooter(`${newState.member.user.tag}'s ID is ${newState.member.user.id} • This event is from a Voice State Update.`)
-          .setTimestamp();
-        return logChannel.send(embed);
-      } else if (voice2 == 'Left') {
-        const embed = new MessageEmbed()
-          .setColor('#D92C2C')
-          .setAuthor(`${newState.member.user.tag} has left a voice channel.`, newState.member.user.displayAvatarURL(), 'https://discord.gg/83SAWkh')
-          .setDescription(`**${oldState.member.user.tag}** left voice channel **${voice1}** in the category **${oldState.channel.parent.name}**.`)
-          .setFooter(`${newState.member.user.tag}'s ID is ${newState.member.user.id} • This event is from a Voice State Update.`)
-          .setTimestamp();
-        return logChannel.send(embed);
-      } else if (voice1 != newState.channel.name) {
-        let newPar;
-        if (oldState.channel.parent.name === newState.channel.parent.name) {newPar = 'same category';} else {newPar = `category **${newState.channel.parent.name}**`;}
-        const embed = new MessageEmbed()
-          .setColor('#006400')
-          .setDescription(`${oldState.member.user.tag} has moved from **${voice1}** in the category **${oldState.channel.parent.name}** to **${newState.channel.name}** in the ${newPar}.`)
-          .setAuthor(`${newState.member.user.tag} has moved to another voice channel.`, newState.member.user.displayAvatarURL(), 'https://discord.gg/83SAWkh')
-          .setFooter(`${newState.member.user.tag}'s ID is ${newState.member.user.id} • This event is from a Voice State Update.`)
-          .setTimestamp();
-        return logChannel.send(embed);
+          if (voice1 == 'None') {
+            const embed = new MessageEmbed()
+              .setColor('#32CD32')
+              .setAuthor(`${newState.member.user.tag} has joined a voice channel.`, newState.member.user.displayAvatarURL(), 'https://discord.gg/83SAWkh')
+              .setDescription(`**${newState.member.user.tag}** joined voice channel **${voice2}** in the category **${newState.channel.parent.name}**.`)
+              .setFooter(`${newState.member.user.tag}'s ID is ${newState.member.user.id} • This event is from a Voice State Update.`)
+              .setTimestamp();
+            return logChannel.send(embed);
+          } else if (voice2 == 'Left') {
+            const embed = new MessageEmbed()
+              .setColor('#D92C2C')
+              .setAuthor(`${newState.member.user.tag} has left a voice channel.`, newState.member.user.displayAvatarURL(), 'https://discord.gg/83SAWkh')
+              .setDescription(`**${oldState.member.user.tag}** left voice channel **${voice1}** in the category **${oldState.channel.parent.name}**.`)
+              .setFooter(`${newState.member.user.tag}'s ID is ${newState.member.user.id} • This event is from a Voice State Update.`)
+              .setTimestamp();
+            return logChannel.send(embed);
+          } else if (voice1 != newState.channel.name) {
+            let newPar;
+            if (oldState.channel.parent.name === newState.channel.parent.name) {newPar = 'same category';} else {newPar = `category **${newState.channel.parent.name}**`;}
+            const embed = new MessageEmbed()
+              .setColor('#006400')
+              .setDescription(`${oldState.member.user.tag} has moved from **${voice1}** in the category **${oldState.channel.parent.name}** to **${newState.channel.name}** in the ${newPar}.`)
+              .setAuthor(`${newState.member.user.tag} has moved to another voice channel.`, newState.member.user.displayAvatarURL(), 'https://discord.gg/83SAWkh')
+              .setFooter(`${newState.member.user.tag}'s ID is ${newState.member.user.id} • This event is from a Voice State Update.`)
+              .setTimestamp();
+            return logChannel.send(embed);
+          }
+        });
+      } else {
+        return;
       }
-    } else {
-      return;
-    }
+    });
   }
 };
