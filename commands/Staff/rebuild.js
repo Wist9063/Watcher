971f6391b6ad78d9917d213d465cdd0382ec8aae
josh <wist9063@gmail.com>
2020-05-12 22:59:59 -0700

    redid rebuild

--- commands/Staff/rebuild.js
+++ commands/Staff/rebuild.js
@@ -11,14 +11,27 @@ module.exports = class extends Command {
   async execute(message) {
     if (message.perm < 9) return;
     const g = this.client.guilds.cache.array();
-    const qdb = require('quick.db');
-    const array = [];
     const f = this.client;
     g.forEach(function(item) {
-      if (qdb.has(`guild_${item.id}.events`)) {
-        array.push(qdb.get(`guild_${item.id}.events`));
-        f.mongod.db('watcher').collection('events').updateOne({gID: item.id}, {$set: {events: {array}}});
-      }
+      f.mongod.db('watcher').collection('events').insertMany([{ 
+        gID: item.id, 
+        events: {
+          channelCreate: false,
+          channelDelete: false,
+          guildBanAdd: false,
+          guildBanRemove: false,
+          guildMemberAdd: false,
+          guildMemberRemove: false,
+          guildMemberUpdate: false,
+          messageDelete: false,
+          messageDeleteBulk: false,
+          messageUpdate: false,
+          voiceStateUpdate: false,
+          messageReactionAdd: false,
+          messageReactionRemove: false,
+          roleCreate: false
+        }
+      }]);
     });
   }
 };
