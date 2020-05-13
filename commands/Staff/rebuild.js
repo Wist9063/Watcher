8c09f70677225d33e33bba5810a558779621253b
josh <wist9063@gmail.com>
2020-05-12 23:19:31 -0700

    forgot to insert

--- commands/Staff/rebuild.js
+++ commands/Staff/rebuild.js
@@ -12,12 +12,26 @@ module.exports = class extends Command {
     if (message.perm < 9) return;
     const g = this.client.guilds.cache.array();
     const f = this.client;
-    const qdb = require('quick.db');
     g.forEach(function(item) {
-      if (qdb.has(`guild_${item.id}.events`)) {
-        const events = qdb.get(`guild_${item.id}.events`);
-        f.mongod.db('watcher').collection('events').updateOne({gID: item.id}, {$set: {events: {events}}});
-      }
+      f.mongod.db('watcher').collection('events').insertMany([{ 
+        gID: item.id, 
+        events: {
+          channelCreate: false,
+          channelDelete: false,
+          guildBanAdd: false,
+          guildBanRemove: false,
+          guildMemberAdd: false,
+          guildMemberRemove: false,
+          guildMemberUpdate: false,
+          messageDelete: false,
+          messageDeleteBulk: false,
+          messageUpdate: false,
+          voiceStateUpdate: false,
+          messageReactionAdd: false,
+          messageReactionRemove: false,
+          roleCreate: false
+        }
+      }]);
     });
   }
 };
