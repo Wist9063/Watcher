cef9eeb9cd9ee7053044f52e9f10567d92c59103
josh <wist9063@gmail.com>
2020-08-09 12:03:23 -0700

    Added more error logging & fixed avatar

--- events/Guild/guildMemberRemove.js
+++ events/Guild/guildMemberRemove.js
@@ -19,14 +19,23 @@ module.exports = class extends BotEvent {
           if (b.wb.wbID === null || b.wb.wbKey === null) return;
           const logChannel = new WebhookClient(b.wb.wbID, b.wb.wbKey);
           if (!logChannel) return;
-
-          const embed = new MessageEmbed()
-            .setColor('#DD5449')
-            .setAuthor(`${member.user.tag} has left the server.`, member.user.displayAvatarURL(), 'https://discord.gg/83SAWkh')
-            .setDescription(` **${guild.name}** now has __${guild.memberCount}__ members.\nThis user joined discord on \`${moment(member.joinedAt).format('MMMM Do, YYYY, h:mm:ss A')} (Universal Coordinated Time)\``)
-            .setFooter(`Watcher Event • User Left | ID: ${member.user.id}`)
-            .setTimestamp();
-          return logChannel.send(embed).catch(e => console.error(e));
+          if (member.deleted) {
+            const embed = new MessageEmbed()
+              .setColor('#DD5449')
+              .setAuthor(`${member.user.tag} has left the server.`, member.user.displayAvatarURL(), 'https://discord.gg/83SAWkh')
+              .setDescription(` **${guild.name}** now has __${guild.memberCount}__ members.\nThis user joined discord on \`${moment(member.joinedAt).format('MMMM Do, YYYY, h:mm:ss A')} (Universal Coordinated Time)\``)
+              .setFooter(`Watcher Event • User Left | ID: ${member.user.id}`)
+              .setTimestamp();
+            return logChannel.send(embed).catch(e => console.error(e));
+          } else {
+            const embed = new MessageEmbed()
+              .setColor('#DD5449')
+              .setAuthor(`${member.user.tag} has left the server.`, member.user.displayAvatarURL(), 'https://discord.gg/83SAWkh')
+              .setDescription(` **${guild.name}** now has __${guild.memberCount}__ members.\nThis user joined discord on \`${moment(member.joinedAt).format('MMMM Do, YYYY, h:mm:ss A')} (Universal Coordinated Time)\``)
+              .setFooter(`Watcher Event • User Left | ID: ${member.user.id}`)
+              .setTimestamp();
+            return logChannel.send(embed).catch(e => console.error(e));
+          }
         });
       } else {
         return;
