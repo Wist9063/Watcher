02a7c95d30aa72bc94ba0730f075265002615ec6
wist9063 <me@joshlol.xyz>
2021-02-06 14:07:07 -0800

    more ping cooldown

--- commands/Utility/ping.js
+++ commands/Utility/ping.js
@@ -5,7 +5,7 @@ module.exports = class extends Command {
   constructor(client, filePath) {
     super(client, filePath, {
       name: 'ping',
-      cooldown: 5,
+      cooldown: 10,
       aliases: []
     });
   }
