b987935375da34eaeed1a36b82ed1dd46059ec85
wist9063 <me@joshlol.xyz>
2021-02-05 19:04:32 -0800

    fixed command perm check

--- handlers/permission.js
+++ handlers/permission.js
@@ -4,13 +4,13 @@ class PermissionLevel {
 
   fetch(user, message) {
     if (config.owners.includes(user.id)) return [10, '<a:verifiedtech:742101827650322443> Owner'];
-    if (config.helpers.includes(user.id)) return [9, '<a:verifiedtech:742101827650322443> Development Team'];
-    if (config.admins.includes(user.id)) return [8, '<a:verifiedtech:742101827650322443> Global Admin'];
-    if (config.mods.includes(user.id)) return [7, '<a:verifiedtech:742101827650322443> Global Mod'];
-    if (user.id === message.guild.ownerID) return [4, 'Guild Owner'];
-    if (message.guild.member(user).hasPermission('ADMINISTRATOR')) return [3, 'Guild Admin'];
-    if (message.guild.member(user).hasPermission('MANAGE_GUILD')) return [2, 'Guild Mod'];
-    return [0 , 'Guild Member'];
+    else if (config.helpers.includes(user.id)) return [9, '<a:verifiedtech:742101827650322443> Development Team'];
+    else if (config.admins.includes(user.id)) return [8, '<a:verifiedtech:742101827650322443> Global Admin'];
+    else if (config.mods.includes(user.id)) return [7, '<a:verifiedtech:742101827650322443> Global Mod'];
+    else if (user.id === message.guild.ownerID) return [4, 'Guild Owner'];
+    else if (message.member.hasPermission('ADMINISTRATOR')) return [3, 'Guild Admin'];
+    else if (message.member.hasPermission('MANAGE_GUILD')) return [2, 'Guild Mod'];
+    else return [0 , 'Guild Member'];
   }
 
 }
