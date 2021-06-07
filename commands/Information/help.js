5d87e72fcaf14172c1bfe3a9cd03a908af3bf77c
wist9063 <me@joshlol.xyz>
2021-06-07 14:11:29 -0700

    Changed help format

--- commands/Information/help.js
+++ commands/Information/help.js
@@ -13,17 +13,15 @@ module.exports = class extends Command {
     const embed = new MessageEmbed()
       .setColor('#428bca')
       .setTitle('Watcher | The Advanced Logging Bot')
-      .setDescription('**Prefix**: `w!`\n\nWatcher provides your server with feature-rich advanced customizable logging features, to enhance your user experience.\n\nEvents we currently support:\nchannelCreate, channelDelete, guildBanAdd, guildBanRemove, guildMemberAdd, guildMemberRemove, guildMemberUpdate & more!\n\n__*Special thanks to Nettle#2222 for donating & keeping the project alive!*__')
+      .setDescription('**Prefix**: `w!`\n**To get started, run `w!setup`.**\nNeed help setting up Watcher? Join our support server [here](https://discord.gg/YyGaApfrTc)!\n\nWatcher provides your server with feature-rich advanced customizable logging features, to enhance your user experience.\n\nEvents we currently support:\nchannelCreate, channelDelete, guildBanAdd, guildBanRemove, guildMemberAdd, guildMemberRemove, guildMemberUpdate & more!\n\n__**Special thanks to Nettle#2222 for donating & keeping the project alive!**__\nCheck out our privacy policy [here](https://www.notion.so/Watcher-Privacy-Policy-270fc96623d84176beac89bc6a20dae4)! \n\n**Available commands are listed below.**')
       .setURL('https://discord.gg/83SAWkh')
       .setThumbnail(this.client.user.displayAvatarURL())
       .addField('Information [6]', '`settings`, `help`, `invite`, `info`, `privacy`')
       .addField('Utility [4]', '`perm`, `ping`, `avatar`, `setup`, `userinfo`, `serverinfo`')
       .addField('Moderation [1]', '`purge`')
       .addField('Settings [16]', '`channelcreate`, `channeldelete`, `all-off`, `all-on`, `guildbanadd`, `guildbanremove`, `guildmemberadd`, `guildmemberremove`, `guildmemberupdate`, `log-channel`, `messagedelete`, `messagebulkdelete`, `messageupdate`, `roleCreate`, `roleDelete`, `voicestateupdate`, `messageReactionAdd`, `messageReactionRemove`')
-      .addField('Support Server', '[https://discord.gg/83SAWkh](https://discord.gg/83SAWkh)')
       .addField('Donate', '[https://www.patreon.com/watcherbot](https://www.patreon.com/watcherbot)')
-      .addField('Setup Guide', 'Run the command `w!setup`.')
-      .setFooter('Made by wist9063. Original Idea by jason.#1234 - move fast break things');
-    return message.channel.send(embed);
+      .setFooter('Made by wist9063. Original Idea by jason.#1234 - yo');
+    return message.reply({embed, allowedMentions: { repliedUser: false }});
   }
 };
