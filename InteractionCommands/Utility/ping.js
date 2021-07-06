2e5edcfb6b1c0c9d5047fa531b4dc63af2ca7c7b
wist9063 <me@joshlol.xyz>
2021-07-06 01:46:59 -0700

    fixed ping

--- InteractionCommands/Utility/ping.js
+++ InteractionCommands/Utility/ping.js
@@ -1,5 +1,4 @@
 const Command = require('../../handlers/InteractionCommand.js');
-const fetch = require('node-fetch');
 
 module.exports = class extends Command {
   constructor(client, filePath) {
@@ -9,10 +8,6 @@ module.exports = class extends Command {
   }
 
   async execute(interaction) {
-    fetch('https://srhpyqt94yxb.statuspage.io/api/v2/status.json')
-      .then(res => res.json())
-      .then(json => {
-        interaction.reply(`:satellite_orbital: Heartbeat: **${Math.floor(this.client.ws.ping)}ms**\n:satellite: Discord Status: __${json.status.description}__`);
-      });
+    await interaction.reply(`:satellite_orbital: Heartbeat: **${Math.floor(this.client.ws.ping)}ms**`);
   }
 };
