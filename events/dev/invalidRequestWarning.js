7df820335aa1eab10debad29170a764eac163573
wist9063 <me@joshlol.xyz>
2021-07-07 12:37:32 -0700

    fixed vars

--- events/dev/invalidRequestWarning.js
+++ events/dev/invalidRequestWarning.js
@@ -8,7 +8,7 @@ module.exports = class extends BotEvent {
     });
   }
 
-  async execute(info) {
-    console.log(`[INC RATELIMIT!] [${moment(new Date).tz('America/Los_Angeles').format('MMMM Do YYYY, h:mm:ss A')}] - Count: ${info.count} | Remaining Time: ${info.remainingTime}`);
+  async execute(invalidRequestWarningData) {
+    console.log(`[INC RATELIMIT!] [${moment(new Date).tz('America/Los_Angeles').format('MMMM Do YYYY, h:mm:ss A')}] - Count: ${invalidRequestWarningData.count} | Remaining Time: ${invalidRequestWarningData.remainingTime}`);
   }
 };
