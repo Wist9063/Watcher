7c429627e73ebb148c1d649f251e569efeb8390f
wist9063 <me@joshlol.xyz>
2021-07-07 13:16:20 -0700

    Added count check to avoid console spam

--- events/dev/invalidRequestWarning.js
+++ events/dev/invalidRequestWarning.js
@@ -9,6 +9,8 @@ module.exports = class extends BotEvent {
   }
 
   async execute(invalidRequestWarningData) {
-    console.log(`[INC RATELIMIT!] [${moment(new Date).tz('America/Los_Angeles').format('MMMM Do YYYY, h:mm:ss A')}] - Count: ${invalidRequestWarningData.count} | Remaining Time: ${invalidRequestWarningData.remainingTime}`);
+    if (invalidRequestWarningData.count) {
+      console.log(`[INC RATELIMIT!] [${moment(new Date).tz('America/Los_Angeles').format('MMMM Do YYYY, h:mm:ss A')}] - Count: ${invalidRequestWarningData.count} | Remaining Time: ${invalidRequestWarningData.remainingTime}`);
+    }
   }
 };
