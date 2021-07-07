336d9379b7d460ecb11e06ef8d24af59e28c275b
wist9063 <me@joshlol.xyz>
2021-07-07 11:01:00 -0700

    added ratelimit tracking

--- events/dev/debug.js
+++ events/dev/debug.js
@@ -9,8 +9,6 @@ module.exports = class extends BotEvent {
   }
 
   async execute(info) {
-    if (process.env.idebug == true) {
-      console.log(`[DEBUG!] [${moment(new Date).tz('America/Los_Angeles').format('MMMM Do YYYY, h:mm:ss A')}] - ${info}`);
-    }
+    console.log(`[DEBUG!] [${moment(new Date).tz('America/Los_Angeles').format('MMMM Do YYYY, h:mm:ss A')}] - ${info}`);
   }
 };
