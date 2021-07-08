3f503e659e1844e6a517acc32d84bc1ad8841570
wist9063 <me@joshlol.xyz>
2021-07-08 14:01:01 -0700

    Fixed member remove typo

--- events/Guild/guildMemberRemove.js
+++ events/Guild/guildMemberRemove.js
@@ -20,23 +20,14 @@ module.exports = class extends BotEvent {
       const logChannel = new WebhookClient(b.wb.wbID, b.wb.wbKey);
       this.eventsend++;
 
-      if (member.deleted) {
-        const embed = new MessageEmbed()
-          .setColor('#DD5449')
-          .setAuthor(`${member.user.tag} has been kicked the server.`, member.user.displayAvatarURL())
-          .setDescription(` **${guild.name}** now has __${guild.memberCount}__ members.\nThis user joined discord on \`${moment(member.joinedAt).format('MMMM Do, YYYY, h:mm:ss A')} (Pacific Standard Time)\``)
-          .setFooter(`Watcher Event • User Kicked | ID: ${member.user.id}`)
-          .setTimestamp();
-        return await logChannel.send({ embeds: [embed] }).catch(e => console.error(e));
-      } else {
-        const embed = new MessageEmbed()
-          .setColor('#DD5449')
-          .setAuthor(`${member.user.tag} has left the server.`, member.user.displayAvatarURL())
-          .setDescription(` **${guild.name}** now has __${guild.memberCount}__ members.\nThis user joined discord on \`${moment(member.joinedAt).format('MMMM Do, YYYY, h:mm:ss A')} (Pacific Standard Time)\``)
-          .setFooter(`Watcher Event • User Left | ID: ${member.user.id}`)
-          .setTimestamp();
-        return await logChannel.send({ embeds: [embed] }).catch(e => console.error(e));
-      }
+
+      const embed = new MessageEmbed()
+        .setColor('#DD5449')
+        .setAuthor(`${member.user.tag} has left the server.`, member.user.displayAvatarURL())
+        .setDescription(` **${guild.name}** now has __${guild.memberCount}__ members.\nThis user joined discord on \`${moment(member.joinedAt).format('MMMM Do, YYYY, h:mm:ss A')} (Pacific Standard Time)\``)
+        .setFooter(`Watcher Event • User Left | ID: ${member.user.id}`)
+        .setTimestamp();
+      return await logChannel.send({ embeds: [embed] }).catch(e => console.error(e));
     } else {
       return;
     }
