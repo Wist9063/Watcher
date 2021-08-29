b442d2781dc4147000ce5a78c06bd20fff823b23
wist9063 <me@joshlol.xyz>
2021-08-28 18:56:06 -0700

    Added webhook ID to unexisting webhook log

--- modules/WebhookSender.js
+++ modules/WebhookSender.js
@@ -1,12 +1,12 @@
 const { WebhookClient } = require('discord.js');
 
-module.exports = async pkg => { 
+module.exports = pkg => { 
   if (!pkg.webhook) return;
   const logChannel = new WebhookClient({id: pkg.webhook.id, token: pkg.webhook.token});
 
   logChannel.send({embeds: [pkg.embed]}).catch( e => {
     if (e.code === '10015') {
-      console.log('found unexisting webhook. deleting...');
+      console.log(`found unexisting webhook. id: ${pkg.webhook.id}`);
     }
   });
 };
